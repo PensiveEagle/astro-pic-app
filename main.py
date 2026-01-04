7609417ece06a3ca488b2b7cf83f4a070fb7ff54
PensiveEagle <halljf96@gmail.com>
2026-01-04 11:28:48 +0000

    Initial set up of main.py and requirements.txt

---
+++ main.py
@@ -0,0 +1,9 @@
+# ========== Import packages ========== #
+import requests
+import os
+
+# ========== Get environment variables ========== #
+nasa_api_key = os.getenv( "NASA_API_KEY" )
+
+# ========== API request ========== #
+
