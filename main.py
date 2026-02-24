caded416ed7d0c495dc211420a05f2436e876cae
PensiveEagle <halljf96@gmail.com>
2026-02-24 16:02:27 +0000

    Added streamlit ui elements

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # ========== Import packages ========== #
 import requests
 import os
+import streamlit as st
 
 # ========== Get environment variables ========== #
 nasa_api_key = os.getenv( "NASA_API_KEY" )
@@ -8,9 +9,13 @@ nasa_api_key = os.getenv( "NASA_API_KEY" )
 # ========== API request ========== #
 response = requests.get( f"https://api.nasa.gov/planetary/apod?api_key={nasa_api_key}" )
 
+data = response.json()
 
+data_title = data["title"]
+data_description = data["explanation"]
+data_image = data["url"]
 
-
-# ========== Test ========== #
-
-print( response.json() )
+# ========== Streamlit ========== #
+st.title( data_title )
+st.image( data_image)
+st.write( data_description )
